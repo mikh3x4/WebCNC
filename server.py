c76b9680d7836e533c8eb50a1751d385e5a482f1
Michal <mikh3x4@gmail.com>
2024-02-22 12:11:27 +0000

    gcode converter

--- server.py
+++ server.py
@@ -1,14 +1,15 @@
 
-from fastapi import FastAPI, HTTPException, Depends, Request
+from fastapi import FastAPI, HTTPException, Depends, Request, UploadFile
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
-from tinydb import TinyDB, Query
+# from tinydb import TinyDB, Query
 from typing import List, Optional
 import uuid
 
 from threading import Lock
 
-from secrets import token_hex
+import uvicorn
+# from secrets import token_hex
 
 from fastapi.responses import HTMLResponse
 
@@ -20,7 +21,7 @@ import tempfile
 app = FastAPI()
 
 
-@app.post("/convert_to_gocde/")
+@app.post("/convert_to_gcode/")
 async def create_upload_file(input_file: UploadFile):
     # input_path = f"temp/{file.filename}"
     # with open(input_path, "wb") as buffer:
@@ -30,17 +31,33 @@ async def create_upload_file(input_file: UploadFile):
     scale_y = "5cm"
 
 # vpype read /Users/mik/Downloads/Drawing\ 1-4.svg trim 1 1 linemerge --tolerance 0.1mm linesort scaleto 16cm 7cm layout  tight gwrite --profile my_own_plotter ~/test/test.gcode
+    content = await input_file.read()
+    print(content)
 
-    outfile = tempfile.SpooledTemporaryFile
-    subprocess.run(["vpype", "read", "-",
+    # outfile = tempfile.SpooledTemporaryFile()
+    result = subprocess.run(["vpype", "--config", "vpype.toml", "read", "-",
                     "linemerge", "--tolerance", "0.1mm",
                     "linesort",
                     "scaleto", scale_x, scale_y,
                     "layout", "tight",
                     "gwrite", "--profile", "my_own_plotter", "-"],
-               capture_output=True, stdin=input_file, stdout = outfile)
+                   input=content, capture_output=True)
+    # gcode = outfile.read()
 
-    return outfile
+    svg_result = subprocess.run(["vpype", "--config", "vpype.toml", "read", "-",
+                    "linemerge", "--tolerance", "0.1mm",
+                    "linesort",
+                    "scaleto", scale_x, scale_y,
+                    "layout", "tight",
+                    "write", "-"],
+                   input=content, capture_output=True)
+
+    gcode = result.stdout
+    svg = svg_result.stdout
+
+    print(gcode)
+
+    return gcode
     # return outfile.read()
     # return {"filename": file.filename}
 
@@ -48,6 +65,13 @@ async def create_upload_file(input_file: UploadFile):
 def read_root():
     return FileResponse("site.html")
 
-app.mount("/", StaticFiles(directory="static"), name="static")
+@app.get("/convert", response_class=FileResponse)
+def read_root():
+    return FileResponse("convert.html")
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+# app.mount("/", StaticFiles(directory="static"), name="static")
 
 
