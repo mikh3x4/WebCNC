47804e9a5bd16cbc4a0305d43f7f922171c6039e
Michal <mikh3x4@gmail.com>
2024-04-25 05:52:00 -0700

    join files

---
+++ penslicer.py
@@ -0,0 +1,124 @@
+config='''
+[gwrite.my_own_plotter]
+unit = "mm"
+document_start = "G90;\\nG10 P0 L20 X0 Y0 Z0;\\n"
+layer_start = ""
+line_start = ""
+# segment_first = """G0 X{x:.4f} Y{y:.4f};
+# M10 P0 S100;\\nG04 P500;\\n"""
+segment_first = """G0 X{x:.4f} Y{y:.4f};
+M10 P0 S100;\\n"""
+segment = """G01 X{x:.4f} Y{y:.4f} F1000\\n"""
+# line_end = """M10 P0 S30;\\nG04 P500;\\n"""
+line_end = """M10 P0 S30;\\n"""
+document_end = "M10 P0 S30\\nG0 X0 Y0"
+invert_y = true
+'''
+
+with open("config.toml", "w") as f:
+    f.write(config)
+
+import asyncio
+loop = asyncio.get_event_loop()
+
+import time
+text = None
+
+from pyscript import when
+
+import vpype_cli
+
+import js
+def handler(loop, context):
+    js.console.error(context.message)
+    raise(context.exception)
+loop.set_exception_handler(handler)
+
+
+async def wrap(a):
+    try:
+        return await a
+    except Exception as e:
+        print(e)
+    
+from js import Uint8Array, File, URL, document
+import io
+from pyodide.ffi.wrappers import add_event_listener
+
+gcode = None
+
+def downloadFile(*args):
+    data = gcode
+    encoded_data = data.encode('utf-8')
+    my_stream = io.BytesIO(encoded_data)
+
+    js_array = Uint8Array.new(len(encoded_data))
+    js_array.assign(my_stream.getbuffer())
+
+    file = File.new([js_array], "unused_file_name.txt", {type: "text/plain"})
+    url = URL.createObjectURL(file)
+
+    hidden_link = document.createElement("a")
+    hidden_link.setAttribute("download", "output.gcode")
+    hidden_link.setAttribute("href", url)
+    hidden_link.click()
+
+
+async def process_file():
+    global gcode
+    file_input = Element("file-upload")
+    uploaded_file = file_input.element.files.item(0)
+    if uploaded_file is None:
+        return
+
+    display("hi", target="output")
+    file_name = uploaded_file.name
+
+    display("done", target="output")
+
+    width = 16
+    height = 9
+
+    if(file_name.split(".")[-1] == "png"):
+        js_array = await uploaded_file.arrayBuffer()
+        text = js_array.to_py().tobytes()
+        input_cmd = "iread input.png"
+        with open("input.png", "wb") as f:
+            f.write(text)
+
+    if(file_name.split(".")[-1] == "gcode"):
+        gcode = await uploaded_file.text()
+        with open("output.gcode", "w") as f:
+            f.write(gcode)
+        dest_elem = js.document.getElementById("output-image")
+        dest_elem.innerHTML = gcode.replace("\n", "<br>")
+        return
+
+    if(file_name.split(".")[-1] == "svg"):
+        text = await uploaded_file.text()
+        input_cmd = "read input.svg"
+        with open("input.svg", "w") as f:
+            f.write(text)
+
+    extra_pipeline = " trim 1 1 "
+    extra_pipeline = ""
+
+    command = input_cmd + extra_pipeline + f" linemerge --tolerance 0.1mm linesort scaleto {width}cm {height}cm layout tight "
+
+    cmd = command + "gwrite --profile my_own_plotter output.gcode"
+    print(cmd)
+
+    vpype_cli.execute(cmd, global_opt="-c config.toml")
+    vpype_cli.execute(command + "write --pen-up output.svg", global_opt="-c config.toml")
+
+    with open("output.svg") as f:
+        svg_as_string = f.read()
+
+    with open("output.gcode") as f:
+        gcode = f.read()
+
+    dest_elem = js.document.getElementById("output-image")
+    dest_elem.innerHTML = svg_as_string
+
+    dest_elem.children[0].height = "100%"
+
---
+++ serial_sender.js
@@ -0,0 +1,226 @@
+
+var port, textEncoder, writableStreamClosed, writer, historyIndex = -1;
+var recv_buffer = [];
+var estop_var = false;
+
+const lineHistory = [];
+const sleep = ms => new Promise(r => setTimeout(r, ms));
+
+
+async function rungcode() {
+console.log("running gcode");
+var fileInput = document.getElementById('fileInput');
+var file = fileInput.files[0];
+if (file) {
+    var file_reader = new FileReader();
+    file_reader.onload = async function(e) {
+
+	console.log("loading file");
+	var content = e.target.result;
+
+	 var lines = content.split(/\r\n|\n/);
+	    //lines.forEach(function(line) {
+
+	isWaitingForOk = true;
+
+	//for (let line of lines) {
+	for (let i = 0; i < lines.length; i++) {
+	    let line = lines[i]; // Get the current line using index
+
+	    while(true){
+		console.log("sending", line);
+		recv_buffer = "";
+		await sendData(line);
+
+		console.log("sent");
+		while (!recv_buffer.includes("ok") && !recv_buffer.includes("error")) {
+		    await sleep(10); // Short sleep to prevent blocking
+		    if(estop_var){
+			break;
+		    }
+		}
+		
+		if(recv_buffer.includes("ok")){
+		    console.log("got ok");
+		    break;
+		}
+		console.log("resending");
+		await sleep(50);
+
+		if(estop_var){
+		    break;
+		}
+
+	    }
+
+	    console.log(line);
+
+	    if(estop_var){
+		break;
+	    }
+	}
+	console.log("STOPPED")
+
+	isWaitingForOk = false;
+
+    };
+    file_reader.readAsText(file);
+} else {
+    alert('Please select a file first.');
+}
+}
+
+
+
+async function connectSerial() {
+try {
+    // Prompt user to select any serial port.
+    port = await navigator.serial.requestPort();
+    await port.open({ baudRate: 115200 });
+    let settings = {};
+
+    if (localStorage.dtrOn == "true") settings.dataTerminalReady = true;
+    if (localStorage.rtsOn == "true") settings.requestToSend = true;
+    if (Object.keys(settings).length > 0) await port.setSignals(settings);
+
+    
+    textEncoder = new TextEncoderStream();
+    writableStreamClosed = textEncoder.readable.pipeTo(port.writable);
+    writer = textEncoder.writable.getWriter();
+    await listenToPort();
+} catch (e){
+    alert("Serial Connection Failed" + e);
+}
+}
+async function sendCharacterNumber() {
+document.getElementById("lineToSend").value = String.fromCharCode(document.getElementById("lineToSend").value);
+}
+async function sendSerialLine() {
+dataToSend = document.getElementById("lineToSend").value;
+lineHistory.unshift(dataToSend);
+historyIndex = -1; // No history entry selected
+
+dataToSend = dataToSend + "\n";
+
+if (document.getElementById("echoOn").checked == true) appendToTerminal("> " + dataToSend);
+await writer.write(dataToSend);
+if (dataToSend.trim().startsWith('\x03')) echo(false);
+document.getElementById("lineToSend").value = "";
+document.getElementById("lineToSend").value = "";
+}
+var commandQueue = [];
+var isWaitingForOk = false;
+
+async function sendData(dataToSend) {
+
+dataToSend = dataToSend + "\n";
+
+if (document.getElementById("echoOn").checked == true) appendToTerminal("> " + dataToSend);
+await writer.write(dataToSend);
+}
+
+async function NEWsendData(dataToSend) {
+// Add commands to the queue instead of sending them directly
+console.log("sending", isWaitingForOk);
+commandQueue.push(dataToSend + "\n");
+processQueue();
+}
+
+async function processQueue() {
+console.log("processing queue");
+console.log("waiting for ok", isWaitingForOk);
+// If we're already processing the queue or it's empty, do nothing
+if (isWaitingForOk || commandQueue.length === 0) {
+    return;
+}
+
+console.log("next command ");
+// Send the next command in the queue
+const dataToSend = commandQueue.shift();
+if (document.getElementById("echoOn").checked == true) appendToTerminal("> " + dataToSend);
+await writer.write(dataToSend);
+isWaitingForOk = true; // Set flag to wait for "ok"
+}
+
+
+
+async function listenToPort() {
+const textDecoder = new TextDecoderStream();
+const readableStreamClosed = port.readable.pipeTo(textDecoder.writable);
+const reader = textDecoder.readable.getReader();
+
+// Listen to data coming from the serial device.
+while (true) {
+
+	const { value, done } = await reader.read();
+	if (done) {
+	    // Allow the serial port to be closed later.
+	    console.log('[readLoop] DONE', done);
+	    reader.releaseLock();
+	    break;
+	}
+	appendToTerminal(value);
+	recv_buffer += value;
+}
+}
+
+async function NEWlistenToPort() {
+const textDecoder = new TextDecoderStream();
+port.readable.pipeTo(textDecoder.writable);
+const reader = textDecoder.readable.getReader();
+while (true) {
+    const { value, done } = await reader.read();
+    if (done) {
+	console.log('[readLoop] DONE', done);
+	reader.releaseLock();
+	break;
+    }
+    console.log("got value");
+    console.log(value);
+    // Check for "ok" response
+
+    appendToTerminal(value);
+     //var lines = value.split(/\r\n|\n/);
+    //lines.forEach(function(line) {
+	//console.log("got line", line);
+	//if ( value.includes("ok")) {
+	    //console.log("processing next line");
+	    //isWaitingForOk = false; // Ready to send the next command
+	    //processQueue(); // Try processing the next command in the queue
+	//}
+    //});
+
+    console.log("got ok");
+    isWaitingForOk = false; // Ready to send the next command
+    processQueue(); // Try processing the next command in the queue
+}
+}
+
+const serialResultsDiv = document.getElementById("serialResults");
+async function appendToTerminal(newStuff) {
+serialResultsDiv.innerHTML += newStuff;
+if (serialResultsDiv.innerHTML.length > 3000) serialResultsDiv.innerHTML = serialResultsDiv.innerHTML.slice(serialResultsDiv.innerHTML.length - 3000);
+
+//scroll down to bottom of div
+serialResultsDiv.scrollTop = serialResultsDiv.scrollHeight;
+}
+function scrollHistory(direction) {
+// Clamp the value between -1 and history length
+historyIndex = Math.max(Math.min(historyIndex + direction, lineHistory.length - 1), -1);
+if (historyIndex >= 0) {
+    document.getElementById("lineToSend").value = lineHistory[historyIndex];
+} else {
+    document.getElementById("lineToSend").value = "";
+}
+}
+document.getElementById("lineToSend").addEventListener("keyup", async function (event) {
+if (event.keyCode === 13) {
+    sendSerialLine();
+} else if (event.keyCode === 38) { // Key up
+    scrollHistory(1);
+} else if (event.keyCode === 40) { // Key down
+    scrollHistory(-1);
+}
+})
+document.getElementById("echoOn").checked = (localStorage.echoOn == "false" ? false : true);
+
